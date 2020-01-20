730919d17ddb54730dacd64b67684a6acd1f8832
Ryujiro Sakai <sakateu@gmail.com>
2020-01-20 18:44:29 +0900

    mysqlを利用、jsonで返す

--- main.go
+++ main.go
@@ -1,22 +1,69 @@
 package main
 
 import (
+  "fmt"
   "net/http"
   "github.com/labstack/echo"
+  "database/sql"
+  _ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+  ID int
+  Name string
+}
+
 func main() {
+  db, err := sql.Open("mysql", "root:password@tcp(godockerDB)/sample_dev")
+  if err != nil {
+    panic(err)
+  }
+
+  err = db.Ping()
+  if err != nil {
+    panic(err)
+  }
+
+  defer db.Close()
+
   e := echo.New()
   e.GET("/sample", func(c echo.Context) error {
+    db, err := sql.Open("mysql", "root:password@tcp(godockerDB)/sample_dev")
+    if err != nil {
+      panic(err)
+    }
+
+    err = db.Ping()
+    if err != nil {
+      panic(err)
+    }
+
+    defer db.Close()
+
+    rows, err := db.Query("SELECT * FROM users")
+    if err != nil {
+      panic(err.Error())
+    }
+    defer rows.Close()
+
+    var user User
+
+    for rows.Next() {
+      err := rows.Scan(&user.ID, &user.Name)
+      if err != nil {
+        panic(err.Error())
+      }
+      fmt.Println(user.ID, user.Name)
+    }
+
+    err = rows.Err()
+    if err != nil {
+      panic(err.Error())
+    }
+
     return c.JSON(
       http.StatusOK,
-      struct {
-        Code int    `json:"code"`
-        Text string `json:"text"`
-      }{
-        Code: http.StatusOK,
-        Text: http.StatusText(http.StatusOK),
-      },
+      user,
     )
   })
   e.Logger.Fatal(e.Start(":8080"))
