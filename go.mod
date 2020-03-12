f8932ae0b66ffcace4ad78d43b4189cb61e6f4e2
Ryujiro Sakai <sakateu@gmail.com>
2020-03-12 19:26:21 +0900

    setup realize

---
+++ go.mod
@@ -0,0 +1,13 @@
+module app
+
+go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/urfave/cli/v2 v2.2.0 // indirect
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20200311171314-f7b00557c8c4 // indirect
+)
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
   "fmt"
   "net/http"
   "github.com/labstack/echo"
+  "github.com/labstack/echo/middleware"
   "database/sql"
   _ "github.com/go-sql-driver/mysql"
 )
@@ -11,6 +12,11 @@ import (
 type User struct {
   ID int
   Name string
+  Token string
+}
+
+type Token struct {
+  Token string `json:"token"`
 }
 
 func main() {
@@ -27,7 +33,74 @@ func main() {
   defer db.Close()
 
   e := echo.New()
-  e.GET("/sample", func(c echo.Context) error {
+  e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+  }))
+
+  e.GET("/api/login", func(c echo.Context) error {
+    var user User
+
+    user.ID = 1
+    user.Name = "test"
+    user.Token = "hotreload test"
+
+    return c.JSON(
+      http.StatusOK,
+      user,
+    )
+  })
+
+  e.POST("/api/login", func(c echo.Context) error {
+    db, err := sql.Open("mysql", "root:password@tcp(godockerDB)/sample_dev")
+    if err != nil {
+      panic(err)
+    }
+
+    err = db.Ping()
+    if err != nil {
+      panic(err)
+    }
+
+    defer db.Close()
+
+    rows, err := db.Query("SELECT * FROM users")
+    if err != nil {
+      panic(err.Error())
+    }
+    defer rows.Close()
+
+    var user User
+
+    var token Token
+
+    // for rows.Next() {
+    //   err := rows.Scan(&user.ID, &user.Name)
+    //   if err != nil {
+    //     panic(err.Error())
+    //   }
+    //   fmt.Println(user.ID, user.Name)
+    // }
+
+    user.ID = 0
+    user.Name = "aaa"
+    token.Token = "sample"
+
+    fmt.Println(token)
+    fmt.Println(user)
+
+    err = rows.Err()
+    if err != nil {
+      panic(err.Error())
+    }
+
+    return c.JSON(
+      http.StatusOK,
+      token,
+    )
+  })
+
+  e.GET("/api/me", func(c echo.Context) error {
     db, err := sql.Open("mysql", "root:password@tcp(godockerDB)/sample_dev")
     if err != nil {
       panic(err)
